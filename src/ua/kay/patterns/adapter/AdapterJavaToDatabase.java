0577778b66908b7756252331a21d1027594b0aa5
Kutsenko Alexander <kucenkoaleksandr@gmail.com>
2017-05-25 17:07:18 +0300

    The implementation of the design pattern "Adapter".

---
+++ src/ua/kay/patterns/adapter/AdapterJavaToDatabase.java
@@ -0,0 +1,24 @@
+package ua.kay.patterns.adapter;
+
+public class AdapterJavaToDatabase extends JavaApplication implements Database {
+
+    @Override
+    public void insert() {
+        saveObject();
+    }
+
+    @Override
+    public void update() {
+        updateObject();
+    }
+
+    @Override
+    public void select() {
+        loadObject();
+    }
+
+    @Override
+    public void remove() {
+        deleteObject();
+    }
+}
---
+++ src/ua/kay/patterns/adapter/Database.java
@@ -0,0 +1,11 @@
+package ua.kay.patterns.adapter;
+
+public interface Database {
+    public void insert();
+
+    public void update();
+
+    public void select();
+
+    public void remove();
+}
